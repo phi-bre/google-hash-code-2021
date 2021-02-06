48bb5ca7cec1168df2a0085865a2bf2b76418f5c
Florian Walter <fw@digio.ch>
2021-02-06 19:50:41 +0100

    added ingredient and pizza scoring

--- src/algorithm.ts
+++ src/algorithm.ts
@@ -1,12 +1,51 @@
-import { from } from 'https://deno.land/x/lazy@v1.7.1/mod.ts';
-import { Input, Output } from './io.ts';
+import {Input, Output, Pizza} from './io.ts';
+
+interface PizzaScore {
+    pizza: Pizza;
+    score: number;
+}
 
 export function algorithm(input: Input, weights: number[]): Output {
+    const pizzaScores = calculatePizzaScores(input.pizzas)
+        .sort((a, b) => b.score - a.score);
+
+    console.log(pizzaScores);
+
     return {
-        num_delivered_pizzas: 2,
-        teams: [
-            { team_size: 2, pizzas: [1, 4] },
-            { team_size: 3, pizzas: [0, 2, 3] },
-        ],
+        num_delivered_pizzas: 0,
+        teams: [],
     };
+}
+
+function calculatePizzaScores(pizzas: Array<Pizza>): Array<PizzaScore> {
+    const ingredientScores = calculateIngredientScores(pizzas);
+
+    return pizzas.map(pizza => {
+        let score = 0;
+        for (const ingredient of pizza.ingredients) {
+            score += ingredientScores.get(ingredient) || 0;
+        }
+
+        return {pizza, score}
+    });
+}
+
+function calculateIngredientScores(pizzas: Array<Pizza>): Map<string, number> {
+    const ingredients = new Map<string, number>();
+    let total = 0;
+
+    for (const pizza of pizzas) {
+        for (const ingredient of pizza.ingredients) {
+            const count = ingredients.get(ingredient) || 0;
+            ingredients.set(ingredient, count + 1);
+            total++;
+        }
+    }
+
+    for (const ingredient of ingredients.keys()) {
+        const count = ingredients.get(ingredient) || 0;
+        ingredients.set(ingredient, count / total);
+    }
+
+    return ingredients;
 }
--- src/io.ts
+++ src/io.ts
@@ -1,13 +1,15 @@
+export interface Pizza {
+    index: number;
+    num_ingredients: number;
+    ingredients: Set<string>;
+}
+
 export interface Input {
     num_available_pizzas: number;
     num_2_person_teams: number;
     num_3_person_teams: number;
     num_4_person_teams: number;
-    pizzas: Array<{
-        index: number;
-        num_ingredients: number;
-        ingredients: Set<string>;
-    }>;
+    pizzas: Array<Pizza>;
 }
 
 export interface Output {
@@ -41,7 +43,7 @@ export function read(file: string): Input {
 
 export function write(file: string, output: Output) {
     let text = `${output.num_delivered_pizzas}\n`;
-    output.teams.forEach(({ team_size, pizzas }) => {
+    output.teams.forEach(({team_size, pizzas}) => {
         text += team_size + ' ' + [...pizzas].join(' ') + '\n';
     });
     Deno.writeTextFile(`out/${file}`, text);
